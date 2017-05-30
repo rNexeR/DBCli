746045f8972060e317ef1cdfa02b88a0660f09ca
rnexer <rnexer@gmail.com>
2017-05-29 23:48:48 -0600

    classlibs, console app created; create database and use database done

---
+++ DBCli/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FileDBCLi;
+
+namespace DBCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cli = new Cli();
+            var current_db = cli.GetCurrentDB();
+
+            while(true){
+                Console.Write($"{current_db}>");
+                var input = Console.ReadLine();
+                var arguments = input.Split(' ');
+                if(input.ToLower() == "exit")
+                    break;
+                cli.Execute(arguments);
+                Console.WriteLine();
+                current_db = cli.GetCurrentDB();
+            }
+
+            cli.Dispose();
+            Console.WriteLine("Bye bye!");
+            Console.Read();
+        }
+    }
+}
